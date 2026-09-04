ac6712d39f9ef11cf2ba75d6f967cc991c664ea0
kagishontita10 <ST10472797@rcconnect.edu.za>
2026-09-04 12:13:40 +0200

    Add Events table with foreign key to users

--- script.sql
+++ script.sql
@@ -6,3 +6,10 @@ create table users(userID int primary key,
               email varchar(50) not null unique); 
  
  
+ 
+create table Events(eventID int primary key,  
+userID int not null, foreign key (userID) references users(userID), 
+EventName varchar(40) not null,  
+description varchar(30),EventDate date not null, 
+location varchar(50) not null,eventType varchar(50) not null ); 
+ 
